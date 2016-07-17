00dbee5154974c3390749e92d8ec8a80b073747e
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 13:30:19 -0500

    Start from scratch

--- client/calendar.js
+++ client/calendar.js
@@ -6,26 +6,4 @@ import { render } from 'react-dom';
 // Import css
 import css from './styles/style.styl';
 
-//Import components
-import App from './components/App';
-import Main from './components/Main';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
-
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
-import { Provider } from 'react-redux';
-import store, { history } from './store';
-
-const router = (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={PhotoGrid}></IndexRoute>
-        <Route path="/view/:postId" component={Single}></Route>
-      </Route>
-    </Router>
-  </Provider>
-)
-
-render(router, document.getElementById('root'));
+render(<p>hello</p>, document.getElementById('root'));

387ad00ba3aa369f866a9fc8db5fba3fb11cd5d6
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 17:34:07 -0500

    Return state as is from currentEvents reducer (to fix later)

--- client/reducers/currentEvents.js
+++ client/reducers/currentEvents.js
@@ -1,9 +1,12 @@
 import moment from 'moment'
 
 function currentEvents(state = [], action) {
+  const startDate = state.startDate
+  const endDate = moment(startDate).add(7, 'days')
+
   switch (action.type) {
     case 'INCREMENT_WEEK':
-
+    case 'DECREMENT_WEEK':
       return state;
     default:
       return state;

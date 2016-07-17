ddef79b9fdc778a1a7da1e44621cd84b6ba2f4e4
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 17:36:30 -0500

    Restructuring events again

--- client/data/events.js
+++ client/data/events.js
@@ -1,44 +1,54 @@
-const events = {
-  '07/10/2016': {
-    'date': '07/10/2016 12:30:00',
+const events = [
+  {
+    'date': '07/10/2016',
+    'time': '12:30:00',
     'description': 'Meeting with the boss'
   },
-  '07/10/2016': {
-    'date': '07/10/2016 19:00:00',
+  {
+    'date': '07/10/2016',
+    'time': '19:00:00',
     'description': 'Practice guitar!'
   },
-  '07/11/2016': {
-    'date': '07/11/2016 20:00:00',
+  {
+    'date': '07/11/2016',
+    'time': '20:00:00',
     'description': 'Gig'
   },
-  '07/16/2016': {
-    'date': '07/16/2016 09:00:00',
+  {
+    'date': '07/16/2016',
+    'time': '09:00:00',
     'description': 'Do the laundry!'
   },
-  '07/17/2016': {
-    'date': '07/17/2016 14:30:00',
+  {
+    'date': '07/17/2016',
+    'time': '14:30:00',
     'description': 'Lunch with coworkers'
   },
-  '07/17/2016': {
-    'date': '07/17/2016 18:00:00',
+  {
+    'date': '07/17/2016',
+    'time': '18:00:00',
     'description': 'A date'
   },
-  '07/20/2016': {
-    'date': '07/20/2016 09:00:00',
+  {
+    'date': '07/20/2016',
+    'time': '09:00:00',
     'description': 'Breakfast with coworkers'
   },
-  '07/21/2016': {
-    'date': '07/21/2016 07:00:00',
+  {
+    'date': '07/21/2016',
+    'time': '07:00:00',
     'description': 'Medical exam'
   },
-  '08/06/2016': {
-    'date': '08/06/2016 09:00:00',
+  {
+    'date': '08/06/2016',
+    'time': '09:00:00',
     'description': 'Brother birthday'
   },
-  '09/10/2016': {
-    'date': '09/10/2016 20:00:00',
+  {
+    'date': '09/10/2016',
+    'time': '20:00:00',
     'description': 'ProgPower baby!'
   }
-};
+];
 
 export default events;

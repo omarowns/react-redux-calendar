457d343dcb3495959b808c4f3ffcd786fa990984
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 17:35:50 -0500

    Render events corresponding to that date

--- client/components/Day.js
+++ client/components/Day.js
@@ -3,17 +3,22 @@ import { Link } from 'react-router'
 import Event from './Event'
 
 const Day = React.createClass({
+  renderEvent(event, date, i) {
+    if (event.date == date.format("MM/DD/YYYY")) {
+      return (
+        <li key={i}>
+          <Event event={event} />
+        </li>
+      )
+    }
+  },
   render() {
     const { date, currentEvents } = this.props
     return (
       <div className="day">
         <h2>{date.format('ddd D MMM')}</h2>
         <ul>
-          {currentEvents.map((e,i) =>
-            <li>
-              <Event key={i} event={event} />
-            </li>
-          )}
+          {currentEvents.map((e,i) => this.renderEvent(e, date, i))}
         </ul>
       </div>
     )

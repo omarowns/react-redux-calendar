61330f573e9d12b3842ec5e4c133c08e9e1b66ab
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 14:18:19 -0500

    Add the events data

---
+++ client/data/events.js
@@ -0,0 +1,44 @@
+const events = [
+  {
+    'date': '07/10/2016 12:30:00',
+    'event': 'Meeting with the boss'
+  },
+  {
+    'date': '07/10/2016 19:00:00',
+    'event': 'Practice guitar!'
+  },
+  {
+    'date': '07/11/2016 20:00:00',
+    'event': 'Gig'
+  },
+  {
+    'date': '07/16/2016 09:00:00',
+    'event': 'Do the laundry!'
+  },
+  {
+    'date': '07/17/2016 14:30:00',
+    'event': 'Lunch with coworkers'
+  },
+  {
+    'date': '07/17/2016 18:00:00',
+    'event': 'A date'
+  },
+  {
+    'date': '07/20/2016 09:00:00',
+    'event': 'Breakfast with coworkers'
+  },
+  {
+    'date': '07/21/2016 07:00:00',
+    'event': 'Medical exam'
+  },
+  {
+    'date': '08/06/2016 09:00:00',
+    'event': 'Brother birthday'
+  },
+  {
+    'date': '09/10/2016 20:00:00',
+    'event': 'ProgPower baby!'
+  }
+];
+
+export default events;

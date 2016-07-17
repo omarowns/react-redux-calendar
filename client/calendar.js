abe0652cbd23a9b36ff0c9aca038ce002ebcb6ad
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 15:09:32 -0500

    Add Provider

--- client/calendar.js
+++ client/calendar.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
 
 // Import css
 import css from './styles/style.styl';
@@ -11,14 +12,18 @@ import Week from './components/Week'
 import Day from './components/Day'
 import Event from './components/Event'
 
+import store, { history } from './store'
+
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Week}></IndexRoute>
-      <Route path="/day/:day" component={Day}></Route>
-      <Route path="/event/:event" component={Event}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={Week}></IndexRoute>
+        <Route path="/day/:day" component={Day}></Route>
+        <Route path="/event/:event" component={Event}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));

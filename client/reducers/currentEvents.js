602aa31cc5cd850baa3bc56081448eb585b29502
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 18:10:33 -0500

    Implement current events reducer

--- client/reducers/currentEvents.js
+++ client/reducers/currentEvents.js
@@ -1,13 +1,12 @@
 import moment from 'moment'
 
 function currentEvents(state = [], action) {
-  const startDate = state.startDate
+  const startDate = action.startDate
   const endDate = moment(startDate).add(7, 'days')
 
   switch (action.type) {
-    case 'INCREMENT_WEEK':
-    case 'DECREMENT_WEEK':
-      return state;
+    case 'REFRESH_EVENTS':
+      return action.events.filter(event => moment(event.date, "MM/DD/YYYY").isBetween(startDate, endDate) );
     default:
       return state;
   }

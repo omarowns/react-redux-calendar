2de39477c9af70d7cb1a2f8f79a738735aa92b10
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 15:10:00 -0500

    Fix moment import and history export

--- client/store.js
+++ client/store.js
@@ -1,19 +1,20 @@
 import { createStore, compse } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
-import * as moment from 'moment'
+import moment from 'moment'
 
 import rootReducer from './reducers/index'
 
 import events from './data/events'
 
+const startDate = moment()
+
 const defaultState = {
   events,
-  startDate: moment()
+  startDate
 }
 
 const store = createStore(rootReducer, defaultState)
-const history = syncHistoryWithStore(browserHistory, store)
+export const history = syncHistoryWithStore(browserHistory, store)
 
 export default store
-export history

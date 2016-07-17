7c606f129e293218b33c2663a159dd75515b4c10
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 16:11:27 -0500

    Show events from current date

--- client/components/Day.js
+++ client/components/Day.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router'
+import Event from './Event'
 
 const Day = React.createClass({
   render() {
+    const { date, currentEvents } = this.props
     return (
       <div className="day">
-        Display the individual day
+        <h2>{date.format('ddd D MMM')}</h2>
+        <ul>
+          {currentEvents.map((e,i) =>
+            <li>
+              <Event key={i} event={event} />
+            </li>
+          )}
+        </ul>
       </div>
     )
   }

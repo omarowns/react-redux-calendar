f454b7b492c247091b70580ff265f80973ef213a
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 13:58:54 -0500

    Set up router

--- client/calendar.js
+++ client/calendar.js
@@ -1,11 +1,24 @@
 // let's go!
-import React from 'react';
-
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 // Import css
 import css from './styles/style.styl';
 
 import Main from './components/Main'
+import Week from './components/Week'
+import Day from './components/Day'
+import Event from './components/Event'
 
-render(<Main/>, document.getElementById('root'));
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Week}></IndexRoute>
+      <Route path="/day/:day" component={Day}></Route>
+      <Route path="/event/:event" component={Event}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));

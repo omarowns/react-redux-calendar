ea77118fd0ba1ebc47971ea4c865ac623f02aa5c
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 16:10:26 -0500

    Add currentEvents reducer to rootReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import events from './events'
 import startDate from './startDate'
+import currentEvents from './currentEvents'
 
-const rootReducer = combineReducers({events, startDate, routing: routerReducer})
+const rootReducer = combineReducers({events, startDate, currentEvents, routing: routerReducer})
 
 export default rootReducer

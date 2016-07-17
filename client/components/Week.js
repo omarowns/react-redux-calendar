fa33687d8c7d3fa30870831b1e88abed8a84d1e1
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 18:01:40 -0500

    Refactor onClick for prev and next week

--- client/components/Week.js
+++ client/components/Week.js
@@ -3,16 +3,26 @@ import Day from './Day'
 import moment from 'moment'
 
 const Week = React.createClass({
+  nextWeek(e) {
+    const {startDate, incrementWeek, refreshCurrentEvents, events} = this.props
+    incrementWeek(startDate);
+    refreshCurrentEvents(events, startDate);
+  },
+  prevWeek(e) {
+    const {startDate, decrementWeek, refreshCurrentEvents, events} = this.props
+    decrementWeek(startDate);
+    refreshCurrentEvents(events, startDate);
+  },
   render() {
     const { startDate } = this.props
     const dates = [0,1,2,3,4,5,6].map((e,i) => moment(startDate).add(e, 'days'))
     return (
       <div className="week">
-        <button onClick={this.props.decrementWeek.bind(null, startDate)}>&lt;&lt;</button>
+        <button onClick={this.prevWeek}>&lt;&lt;</button>
           {dates.map((e,i) =>
             <Day {...this.props} key={i} date={e}/>
           )}
-        <button onClick={this.props.incrementWeek.bind(null, startDate)}>&gt;&gt;</button>
+        <button onClick={this.nextWeek}>&gt;&gt;</button>
       </div>
     )
   }

3203448360a618dce017409e7ab6d9b990f0589b
Omar Garcia <omar.garcia@magmalabs.io>
2016-07-17 16:10:55 -0500

    Display days of the week on the Week component

--- client/components/Week.js
+++ client/components/Week.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Day from './Day'
+import moment from 'moment'
 
 const Week = React.createClass({
   render() {
+    const { startDate } = this.props
+    const dates = [0,1,2,3,4,5,6].map((e,i) => moment(startDate).add(e, 'days'))
     return (
       <div className="week">
-        Display the whole week
+        <button>&lt;&lt;</button>
+          {dates.map((e,i) =>
+            <Day {...this.props} key={i} date={e}/>
+          )}
+        <button>&gt;&gt;</button>
       </div>
     )
   }
